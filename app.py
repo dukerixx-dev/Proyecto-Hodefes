7ebfa1e9fdc1ba668a4332bbfb6f4875c310cb41
dukerixx-dev <dukerixx@gmail.com>
2026-06-04 08:50:43 -0400

    eliminacion del layout.css para facilitar la distribuicion de los archivos

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
+import json
 
 app=Flask(__name__)
 
 @app.route('/')
+def inicio_sesion():
+        
+    return render_template('pagina inicio sesion.html')
+
+@app.route('/pagina_horarios/')
 def pagina_horarios():
+
     return render_template('pagina horarios.html')
 
-@app.route('/inicio_sesion/')
-def inicio_sesion():
-    return render_template('pagina inicio sesion.html')
 
 @app.route('/pagina_docentes/')
 def pagina_docente():
+
     return render_template('pagina docentes.html')
 
 
