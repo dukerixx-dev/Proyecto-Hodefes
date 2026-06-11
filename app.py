6bcb4fa41bb7968b7c06e999637d88bb2800a2ff
dukerixx-dev <dukerixx@gmail.com>
2026-06-11 10:26:27 -0400

    listo el inicio de sesion, pero falta especificar si inicio sesion un estudiante o un docente

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app=Flask(__name__)
 
-@app.route('/')
-def inicio_sesion():
-        
-    return render_template('pagina inicio sesion.html')
+
+    
+@app.post("/login")
+def verificar_inicio_de_sesion():
+    correo=request.form.get("correo")
+    contrasena=request.form.get("contraseña")
+    with open("inicio_sesion.json","r") as archivo:
+            inicio_sesion=json.load(archivo)
+    for i in inicio_sesion:
+        if correo == i["correo"] and contrasena == i["contrasena"]:
+            return redirect(url_for("pagina_horarios"))
+
+    return render_template("pagina inicio sesion.html ")
 
 
-#def contenido_horarios():
-
+@app.get('/')
+def inicio_de_sesion():
+    return render_template("pagina inicio sesion.html ")
+    
 
 @app.route('/pagina_horarios/')
 def pagina_horarios():
-    #el profe lucas sugiere hacer otra funcion que busque o filtre a un diccionario especifico que serian los profesores en el json para colocarlo en el html
     with open("profes.json","r") as archivo:
         profes=json.load(archivo)
     return render_template('pagina horarios.html',datos=profes)
+
+#def filtro_y_busqueda():
+    #investigar sobre post, ida y vuelta
 
 
 @app.route('/pagina_docentes/')
