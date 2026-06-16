9f63bcdfe457b727d11bc84479cde3227b33cd43
dukerixx-dev <dukerixx@gmail.com>
2026-06-16 12:36:13 -0400

    inicio de sesion de estudiantes, docentes e invitados

--- app.py
+++ app.py
@@ -3,25 +3,32 @@ import json
 
 app=Flask(__name__)
 
+@app.get('/')
+def inicio_de_sesion():
+    return render_template("pagina inicio sesion.html ")
 
-    
 @app.post("/login")
 def verificar_inicio_de_sesion():
     correo=request.form.get("correo")
     contrasena=request.form.get("contraseña")
     with open("inicio_sesion.json","r") as archivo:
             inicio_sesion=json.load(archivo)
+
     for i in inicio_sesion:
         if correo == i["correo"] and contrasena == i["contrasena"]:
-            return redirect(url_for("pagina_horarios"))
-
+            #subcadena desde el final del correo hasta el arroba
+            if i["correo"][-17:]=="alumnos.ulagos.cl":
+                print(i["correo"][-17:])
+                return redirect(url_for("pagina_horarios"))
+            else:   
+                return redirect(url_for("pagina_docentes"))
     return render_template("pagina inicio sesion.html ")
 
-
-@app.get('/')
-def inicio_de_sesion():
-    return render_template("pagina inicio sesion.html ")
-    
+@app.route("/pagina_horarios/invitado")    
+def invitado():
+    with open("profes.json","r") as archivo:
+        profes=json.load(archivo)
+    return render_template('pagina de horarios invitados.html',datos=profes)
 
 @app.route('/pagina_horarios/')
 def pagina_horarios():
@@ -29,13 +36,8 @@ def pagina_horarios():
         profes=json.load(archivo)
     return render_template('pagina horarios.html',datos=profes)
 
-#def filtro_y_busqueda():
-    #investigar sobre post, ida y vuelta
-
-
 @app.route('/pagina_docentes/')
-def pagina_docente():
-
+def pagina_docentes():
     return render_template('pagina docentes.html')
 
 
